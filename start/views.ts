2d1f2c4a0f6a528418ff1b8b3fc25229aafdee61
rueljessica <andreia_ruel@hotmail.com>
2023-05-08 21:00:03 -0300

    Inserindo dark mode

--- start/views.ts
+++ start/views.ts
@@ -13,3 +13,5 @@ import View from '@ioc:Adonis/Core/View'
 View.global('logo', 'https://i.ibb.co/Y38MJH7/logo.png')
 
 View.global('tecoteco', 'https://i.ibb.co/m5rV9tM/png-logo.png')
+
+View.global('typo', 'https://i.ibb.co/SKk9kNd/png-typo.png')
