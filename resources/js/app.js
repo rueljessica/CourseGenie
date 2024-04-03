ca18df7aa144b370d30eef00319d6514b6168122
rueljessica <jessruel00@gmail.com>
2024-04-03 12:39:21 -0300

    Adicionando toast (warn) para alteração de senha

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,12 @@
 /* eslint-disable prettier/prettier */
-import Alpine from 'alpinejs'
 import '../css/app.css'
 import { Dropdown } from 'flowbite'
-import { Accordion } from 'flowbite';
 
-Alpine.start()
-
-// Dark mode
+//Dark mode
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon')
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon')
 
-// Change the icons inside the button based on previous settings
+//Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   themeToggleLightIcon.classList.remove('hidden')
 } else {
@@ -44,82 +40,76 @@ themeToggleBtn.addEventListener('click', function () {
       localStorage.setItem('color-theme', 'dark')
     }
   }
-})
+});
 
-  // Dropdown user menu
+//Esqueci senha
+const confirmBtn = document.getElementById("confirm-btn")
+const password = document.getElementById("password")
+const confirmPassword = document.getElementById("confirm-password")
 
-  // set the dropdown menu element
-  const $targetEl = document.getElementById('dropdownMenu')
-
-  // set the element that trigger the dropdown menu on click
-  const $triggerEl = document.getElementById('dropdownButton')
-
-
-
-  // Accordion
-  const accordion = new Accordion(accordionItems, options);
-
-  // create an array of objects with the id, trigger element (eg. button), and the content element
-  const accordionItems = [
-  {
-    id: 'accordion-example-heading-1',
-    triggerEl: document.querySelector('#accordion-example-heading-1'),
-    targetEl: document.querySelector('#accordion-example-body-1'),
-    active: true
-  },
-  {
-    id: 'accordion-example-heading-2',
-    triggerEl: document.querySelector('#accordion-example-heading-2'),
-    targetEl: document.querySelector('#accordion-example-body-2'),
-    active: false
-  },
-  {
-    id: 'accordion-example-heading-3',
-    triggerEl: document.querySelector('#accordion-example-heading-3'),
-    targetEl: document.querySelector('#accordion-example-body-3'),
-    active: false
-  },
-  {
-    id: 'accordion-example-heading-4',
-    triggerEl: document.querySelector('#accordion-example-heading-4'),
-    targetEl: document.querySelector('#accordion-example-body-4'),
-    active: false
-  },
-  {
-    id: 'accordion-example-heading-5',
-    triggerEl: document.querySelector('#accordion-example-heading-5'),
-    targetEl: document.querySelector('#accordion-example-body-5'),
-    active: false
-  },
-  {
-    id: 'accordion-example-heading-6',
-    triggerEl: document.querySelector('#accordion-example-heading-6'),
-    targetEl: document.querySelector('#accordion-example-body-6'),
-    active: false
-  },
-  {
-    id: 'accordion-example-heading-7',
-    triggerEl: document.querySelector('#accordion-example-heading-7'),
-    targetEl: document.querySelector('#accordion-example-body-7'),
-    active: false
+confirmBtn.addEventListener('click', function () {
+  // Verifica se o campo de senha está vazio
+  if (password.value === "") {
+    Toastify({
+      text: "Digite uma senha.",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+    return;
   }
-];
 
-  // options with default values
-  const options = {
-  alwaysOpen: true,
-  activeClasses: 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white',
-  inactiveClasses: 'text-gray-500 dark:text-gray-400',
-  onOpen: (item) => {
-    console.log('accordion item has been shown');
-    console.log(item);
-  },
-  onClose: (item) => {
-    console.log('accordion item has been hidden');
-    console.log(item);
-  },
-  onToggle: (item) => {
-    console.log('accordion item has been toggled');
-    console.log(item);
-  },
-};  
+  // Verifica se as senhas são diferentes
+  if (password.value !== confirmPassword.value) {
+    Toastify({
+      text: "As senhas não conferem.",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+    return;
+  }
+
+  // Senhas são iguais
+  Toastify({
+    text: "Senha alterada com sucesso.",
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    stopOnFocus: true,
+    style: {
+      background: "green",
+    },
+  }).showToast();
+});
+
+//Resetar senha
+const resetConfirmBtn = document.getElementById("reset-confirm-btn");
+
+resetConfirmBtn.addEventListener('click', function () { 
+  if (password.value === "") {
+    Toastify({
+      text: "Digite uma senha.",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+      style: {
+        background: "red",
+      },
+    }).showToast();
+    return;
+  }
+});
