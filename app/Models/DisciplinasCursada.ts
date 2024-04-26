6e52c505f3e5700aef28f939550fe8303fae8d2c
araujoaline07 <araujoaline017@gmail.com>
2024-04-26 19:49:36 -0300

    adicionando funcionalidade para adição e exclusão de disciplinas cursadas

--- app/Models/DisciplinasCursada.ts
+++ app/Models/DisciplinasCursada.ts
@@ -3,6 +3,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 import Disciplina from './Disciplina'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export default class DisciplinasCursada extends BaseModel {
   @column({ isPrimary: true })
@@ -33,10 +34,10 @@ export default class DisciplinasCursada extends BaseModel {
   public userId: number
 
   @column()
-  public equivalenciaId: number
+  public equivalenciaId: number | null
 
   @column()
-  public cargaHoraria: number
+  public cargaHoraria: number | null
 
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
@@ -49,4 +50,26 @@ export default class DisciplinasCursada extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  public static schema = schema.create({
+    nome: schema.string(),
+    codigo: schema.string(),
+    situacao: schema.enum([
+      'APR', 'APRN', 'CANC', 'DISP', 'MATRICULADO', 'MATR', 'REC',
+      'REP', 'REPF', 'REPMF', 'REPN', 'REPNF', 'TRANCADO', 'TRANC',
+      'TRANS', 'INCORP', 'CUMP'
+    ]),
+    ano: schema.string({}, [
+      rules.regex(/^\d{4}\.\d$/)
+    ]),
+    professor: schema.string(),
+    media: schema.number(),
+    tipo: schema.enum(['AA', 'OB', 'EQOB', 'EQOP', 'OP', 'EL']),
+    cargaHoraria: schema.number.optional([
+      rules.requiredWhen('tipo', '=', 'EL')
+    ]),
+    equivalenciaId: schema.number.optional([
+      rules.requiredWhen('tipo', '=', ['EQOB', 'EQOP'])
+    ])
+  })
 }
