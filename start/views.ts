c85048f363873384997c76323dd3703d0f49d007
rueljessica <andreia_ruel@hotmail.com>
2024-06-15 12:19:53 -0300

    Alterações no front

--- start/views.ts
+++ start/views.ts
@@ -18,3 +18,5 @@ View.global('tecoteco', 'https://i.ibb.co/m5rV9tM/png-logo.png')
 View.global('typo', 'https://i.ibb.co/SKk9kNd/png-typo.png')
 
 View.global('icon', 'https://www.svgrepo.com/show/418897/education-graduate-learning.svg')
+
+View.global('aline', 'https://i.ibb.co/HPsbhs9/369717513-3624488494445666-3774639069044985948-n.jpg')
